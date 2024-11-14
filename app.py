3fc3db07b837ffef72614c1ab1522aa49dc2c5c7
Joanne Le <joanne.t.le01@sjsu.edu>
2024-11-13 18:38:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import feature3
 st.image("AquaTech-removebg-preview.png", width=200)
 selected = option_menu(
     menu_title="",
-    options=["Water Testing Information Hub", "Water Quality Testing", "WATER TESTING GUIDE"],
+    options=["Water Testing Kit Locator", "Water Quality Testing", "Water Testing Guide"],
     icons=["", "", ""],
     menu_icon="",
     default_index=0,
@@ -19,7 +19,7 @@ selected = option_menu(
     styles={
         "container": {"padding": "0!important", "width": "100%", "background-color": "#247BA0"},
         "icon": {"color": "white", "font-size": "20px"},
-        "nav-link": {"color": "white", "font-size": "18px", "text-align": "center", "margin": "0px", "--hover-color": "#caf0f8"},
+        "nav-link": {"color": "white", "font-size": "18px", "text-align": "center", "margin": "0px", "--hover-color": "#006494"},
         "nav-link-selected": {"background-color": "#006494"},
     }
 )
